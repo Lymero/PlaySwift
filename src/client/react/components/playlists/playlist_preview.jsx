1f5408b2ff2f2b2a7065714a5fed8f70b7dfe061
ChristopherCastel <castelchristopher95@gmail.com>
2018-12-06 10:15:08 +0100

    likes/dislikes components

--- src/client/react/components/playlists/playlist_preview.jsx
+++ src/client/react/components/playlists/playlist_preview.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Row, Col, ButtonToolbar, Button } from "react-bootstrap";
+import Reactions from "../reactions/reactions_component";
 
 const PlaylistPreview = ({ playlist }) => {
   return (
@@ -11,7 +12,10 @@ const PlaylistPreview = ({ playlist }) => {
       <p>id_tag: {playlist["id_tag"]}</p>
       <p>id_user: {playlist["id_user"]}</p>
       <p>last_update_date: {playlist["last_update_date"]}</p>
-      <p>likes_number: {playlist["likes_number"]}</p>
+      <Reactions
+        likes_number={playlist["likes_number"]}
+        dislikes_number={playlist["dislikes_number"]}
+      />
       <Button>Full playlist !</Button>
     </div>
   );
