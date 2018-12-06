7ca9f8c3c51fef2120fefc9342d9c7a085b3bf15
Florian <florianverdonck@gmail.com>
2018-12-06 15:18:05 +0100

    J'ai merdeyyyyy

--- src/client/react/components/routes/router_outlet.jsx
+++ src/client/react/components/routes/router_outlet.jsx
@@ -1,15 +1,9 @@
 import React from "react";
 import { Route, withRouter } from "react-router-dom";
-<<<<<<< HEAD
-import PlaylistsContainer from "../../containers/playlists/playlists_container";
-import LoginContainer from "react/components/auth/login_container";
-import CallbackContainer from "react/components/auth/callback_container";
-=======
 import AllPlaylistsContainer from "../../containers/playlists/all_playlists_container";
 import MyPlaylistsContainer from "../../containers/playlists/my_playlists_container";
 import LoginContainer from "../auth/login_container";
 import CallbackContainer from "../auth/callback_container";
->>>>>>> f4222fd1b9d6fe6dae40163f0baa994aec2050a6
 
 function RouterOutlet() {
   return (
