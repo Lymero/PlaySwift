b5deb170b7f783c67c3b9ab30348ff71d08b2fb0
ChristopherCastel <castelchristopher95@gmail.com>
2018-12-06 17:41:32 +0100

    redirections ko

--- src/client/react/components/routes/router_outlet.jsx
+++ src/client/react/components/routes/router_outlet.jsx
@@ -1,21 +1,31 @@
 import React from "react";
-import { Route, withRouter } from "react-router-dom";
+import { Route, withRouter, Redirect } from "react-router-dom";
 import AllPlaylistsContainer from "../../containers/playlists/all_playlists_container";
 import MyPlaylistsContainer from "../../containers/playlists/my_playlists_container";
 import LoginContainer from "../auth/login_container";
 import CallbackContainer from "../auth/callback_container";
+import { connect } from "react-redux";
 
-function RouterOutlet() {
+function RouterOutlet({ authState, location: { pathname } }) {
+  const redirectToLogin = !authState.authenticated && pathname !== "/login";
+
   return (
     <React.Fragment>
-      {/* check authenticated + redirect */}
-      {/* routes */}
-      <Route path="/" component={AllPlaylistsContainer} />
+      {/*  {redirectToLogin && <Redirect to="/login" />}
+      {!redirectToLogin && (
+        <React.Fragment> */}
+      <Route exact path="/" component={AllPlaylistsContainer} />
       <Route path="/login" component={LoginContainer} />
       <Route path="/authCallback" component={CallbackContainer} />
       <Route path="/playlists" component={MyPlaylistsContainer} />
+      {/* </React.Fragment>
+      )} */}
     </React.Fragment>
   );
 }
 
-export default withRouter(RouterOutlet);
+const mapStateToProps = state => ({
+  authState: state.usersSession.authState
+});
+
+export default connect(mapStateToProps)(withRouter(RouterOutlet));
