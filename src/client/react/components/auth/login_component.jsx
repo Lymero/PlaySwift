c3345f64bf6d2c615a4db3bea6cf431d7cfdbf4b
ChristopherCastel <castelchristopher95@gmail.com>
2018-12-06 16:21:09 +0100

    quick fix

--- src/client/react/components/auth/login_component.jsx
+++ src/client/react/components/auth/login_component.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Container, Button } from "react-bootstrap";
-import Auth from 'react/services/auth0.js';
+import Auth from "react/services/auth0.js";
 
 const LoginComponent = () => {
   return (
     <Container>
-        <Button bsStyle="info" onClick={ Auth.show() }>Info</Button>
+      <Button bsstyle="info" onClick={Auth.show()}>
+        Info
+      </Button>
     </Container>
   );
 };
