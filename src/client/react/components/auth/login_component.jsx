4235a46d02d3a43677c0e78a4a9e811bf5322474
ChristopherCastel <castelchristopher95@gmail.com>
2018-12-06 16:22:18 +0100

    Revert "quick fix"
    This reverts commit c3345f64bf6d2c615a4db3bea6cf431d7cfdbf4b.

--- src/client/react/components/auth/login_component.jsx
+++ src/client/react/components/auth/login_component.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { Container, Button } from "react-bootstrap";
-import Auth from "react/services/auth0.js";
+import Auth from 'react/services/auth0.js';
 
 const LoginComponent = () => {
   return (
     <Container>
-      <Button bsstyle="info" onClick={Auth.show()}>
-        Info
-      </Button>
+        <Button bsStyle="info" onClick={ Auth.show() }>Info</Button>
     </Container>
   );
 };
