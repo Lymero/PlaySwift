f4df081e3c48216b952641b5ac56a69203363291
ORL-senpai <robin.louis@student.vinci.be>
2018-12-06 10:31:32 +0100

    POST : /playlists + .env

--- src/server/routes/playlists.js
+++ src/server/routes/playlists.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const logger = require("../modules/logger").logger;
 const { db } = require("../modules/db");
 
-router.get("/", async (req, res, next) => {
+router.get("/", async res => {
   try {
     await db.connect();
     const result = await db.query("select * from playswift.playlists");
@@ -12,5 +12,26 @@ router.get("/", async (req, res, next) => {
     logger.info(err.stack);
   }
 });
-//git merge fdp va
+
+router.post("/", async (req, res) => {
+  const text =
+    "insert into playswift.playlists values(default, $1, $2, $3, $4, default, default, $5, default, default) returning id_playlist,name,id_tag,visibile,id_user,creation_date,last_update_date,description,likes_number,dislikes_number";
+
+  const values = [
+    req.body.name,
+    req.body.id_tag,
+    req.body.visible,
+    req.body.id_user,
+    req.body.description
+  ];
+  try {
+    await db.connect();
+    const result = await db.query(text, values);
+    res.send(result.rows[0]);
+    logger.info(text + values);
+  } catch (err) {
+    logger.info(err.stack);
+  }
+});
+
 module.exports = router;
