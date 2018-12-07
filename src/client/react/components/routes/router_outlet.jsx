4232f846bd7d70d6279f635e55f4f7fc5e58279e
martoufff <martoufff@hotmail.com>
2018-12-07 09:40:11 +0100

    router wip

--- src/client/react/components/routes/router_outlet.jsx
+++ src/client/react/components/routes/router_outlet.jsx
@@ -2,26 +2,35 @@ import React from "react";
 import { Route, withRouter, Redirect } from "react-router-dom";
 import AllPlaylistsContainer from "../../containers/playlists/all_playlists_container";
 import MyPlaylistsContainer from "../../containers/playlists/my_playlists_container";
-import LoginContainer from "../auth/login_container";
+import LoginPrompt from "../auth/login_prompt";
 import CallbackContainer from "../auth/callback_container";
 import { connect } from "react-redux";
 
-function RouterOutlet({ authState, location: { pathname } }) {
-  const redirectToLogin = !authState.authenticated && pathname !== "/login";
+import Auth from "react/services/auth0.js";
 
-  return (
-    <React.Fragment>
-      {/*  {redirectToLogin && <Redirect to="/login" />}
-      {!redirectToLogin && (
-        <React.Fragment> */}
-      <Route exact path="/" component={AllPlaylistsContainer} />
-      <Route path="/login" component={LoginContainer} />
-      <Route path="/authCallback" component={CallbackContainer} />
-      <Route path="/playlists" component={MyPlaylistsContainer} />
-      {/* </React.Fragment>
-      )} */}
-    </React.Fragment>
-  );
+const authenticated = Auth.isAuthenticated();
+
+function RouterOutlet({ authState, location: { pathname } }) {
+  const redirectToLogin = !authenticated && pathname !== "/login";
+
+  console.log(authenticated + " " + redirectToLogin);
+
+  if (false) {
+    return (
+      <React.Fragment>
+        <Redirect to="/login" />
+      </React.Fragment>
+    );
+  } else {
+    return (
+      <React.Fragment>
+        <Route exact path="/" component={AllPlaylistsContainer} />
+        <Route path="/login" component={LoginPrompt} />
+        <Route path="/authCallback" component={CallbackContainer} />
+        <Route path="/playlists" component={MyPlaylistsContainer} />
+      </React.Fragment>
+    );
+  }
 }
 
 const mapStateToProps = state => ({
