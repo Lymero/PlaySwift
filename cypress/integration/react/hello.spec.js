dab1a37f3a8dfaac528282b0a8b02acd71f6cce5
Martoufff <martoufff@hotmail.com>
2018-12-08 18:27:38 +0100

    not valid anymore

--- cypress/integration/react/hello.spec.js
+++
@@ -1,8 +0,0 @@
-describe("Playlists route", function() {
-  it("Shows Hello World", function() {
-    cy.visit("/#/playlists/");
-    cy.get("p").should($p => {
-      expect($p.first()).to.contain("hello world");
-    });
-  });
-});
