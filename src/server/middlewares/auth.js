82b6ba972cde9e66ffd7f0ea6f15895215e1a141
ChristopherCastel <castelchristopher95@gmail.com>
2018-12-10 09:54:11 +0100

    id_user added to req.body

--- src/server/middlewares/auth.js
+++ src/server/middlewares/auth.js
@@ -2,21 +2,21 @@ const jwt = require("express-jwt");
 const jwksRsa = require("jwks-rsa");
 
 const checkJwt = jwt({
-    secret: jwksRsa.expressJwtSecret({
-        cache: true,
-        rateLimit: true,
-        jwksRequestsPerMinute: 5,
-        jwksUri: `https://web3.eu.auth0.com/.well-known/jwks.json`
-    }),
-    audience: "http://localhost:3030",
-    issuer: `https://web3.eu.auth0.com/`,
-    algorithms: ["RS256"]
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://web3.eu.auth0.com/.well-known/jwks.json`
+  }),
+  audience: "http://localhost:3030",
+  issuer: `https://web3.eu.auth0.com/`,
+  algorithms: ["RS256"]
 });
 
 const currentUser = (req, res, next) => {
-    req.id_user = req.user.sub.split("|")[1];
-    next();
-}
+  req.body.id_user = req.user.sub.split("|")[1];
+  next();
+};
 
 exports.checkJwt = checkJwt;
 exports.currentUser = currentUser;
