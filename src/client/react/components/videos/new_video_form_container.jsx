d99970f3ba898a7d5ecdfdafd3a0061fa59520f5
Valrani <antoinederoose@yahoo.fr>
2018-12-10 11:29:06 +0100

    add video wip

--- src/client/react/components/videos/new_video_form_container.jsx
+++ src/client/react/components/videos/new_video_form_container.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import NewVideoComponent from "react/components/videos/new_video_component";
+import Api from "react/utils/api";
 
 class NewVideoContainer extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
-      url: "",
-      description: ""
+      url_video: "https://www.youtube.com/watch?v=zGW7TRtcDeQ",
+      description: "issou"
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -19,13 +19,25 @@ class NewVideoContainer extends React.Component {
   }
 
   addVideo(event) {
-    // TODO
+    event.preventDefault();
+    console.log("<<<addVideo>>>");
+    let id_playlist = this.props.id_playlist;
+    let body = {
+      url_video: this.state.url_video,
+      description: this.state.description,
+      id_playlist: this.props.id_playlist
+    };
+    Api({
+      url: `/api/playlists/${id_playlist}/videos`,
+      method: "POST",
+      params: body
+    });
   }
 
   render() {
     return (
       <NewVideoComponent
-        url={this.state.url}
+        url={this.state.url_video}
         description={this.state.description}
         handleChange={this.handleChange}
         addVideo={this.addVideo}
