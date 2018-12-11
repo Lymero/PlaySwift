abd7eba2f84c4d3a6b0d66f6e4d5b1445d37517f
martoufff <martoufff@hotmail.com>
2018-12-11 13:58:16 +0100

    player wip

--- src/client/react/components/player/video_player_root_container.jsx
+++ src/client/react/components/player/video_player_root_container.jsx
@@ -3,31 +3,32 @@ import { Container, Row, Col, Button, ListGroup } from "react-bootstrap";
 import VideoPlayer from "./video_player_container";
 import NewVideo from "react/components/playlists/new_video/new_video_container";
 import SuggestVideoContainer from "react/components/player/suggest_video/suggest_video_container";
+import { withPlaylists } from "react/context/playlists";
+
 class PlayerComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       playlistId: parseInt(location.href.split("/").pop()),
-      videos: [
-        "https://www.youtube.com/watch?v=NA1LM2ucrDc",
-        "https://www.youtube.com/watch?v=XdCOymlUxBc",
-        "https://www.youtube.com/watch?v=fQU0MBft1lY"
-      ],
-      selectedVideo: "https://www.youtube.com/watch?v=NA1LM2ucrDc"
+      selectedVideo: ""
     };
     this.changeVideo = this.changeVideo.bind(this);
+    this.ctxSetCurrentPlaylist = this.props.setCurrentPlaylist;
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    this.ctxSetCurrentPlaylist(this.state.playlistId);
+    
+    this.setState((state, props) => ({
+      selectedVideo: this.props.currentPlaylistVideos[0]
+    }));
+  }
 
   changeVideo(event) {
-    console.log("Root");
-    // console.log(event.target.tagName);
     if (event.target.tagName === "BUTTON") {
-      let url = event.target.dataset.url;
-      console.log(url);
+      let id = event.target.dataset.videoid;
       this.setState((state, props) => ({
-        selectedVideo: url
+        selectedVideo: this.props.currentPlaylistVideos[id]
       }));
     }
   }
@@ -39,13 +40,15 @@ class PlayerComponent extends React.Component {
         <NewVideo id={this.state.playlistId} />
         <ListGroup onClick={this.changeVideo}>
           <Row>
-            {this.state.videos.map((video, i) => (
-              <Col xs={12} key={i}>
-                <ListGroup.Item key={i}>
-                  <Button data-url={video}>{video}</Button>
-                </ListGroup.Item>
-              </Col>
-            ))}
+            {this.props.currentPlaylistVideos !== undefined &&
+              this.props.currentPlaylistVideos.map((video, i) => (
+                <Col xs={12} key={i}>
+                  <ListGroup.Item key={i}>
+                    <Button data-videoid={i}>Play</Button>
+                    <span>{video.description}</span>
+                  </ListGroup.Item>
+                </Col>
+              ))}
           </Row>
         </ListGroup>
         {/* if i'm not the owner of the playlist */}
@@ -55,4 +58,4 @@ class PlayerComponent extends React.Component {
   }
 }
 
-export default PlayerComponent;
+export default withPlaylists(PlayerComponent);
