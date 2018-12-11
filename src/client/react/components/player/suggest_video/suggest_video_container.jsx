3f137ba032b9e72ba2c386bf053792572cc5f72c
Valrani <antoinederoose@yahoo.fr>
2018-12-11 10:58:11 +0100

    suggest video - wip

--- src/client/react/components/player/suggest_video/suggest_video_container.jsx
+++ src/client/react/components/player/suggest_video/suggest_video_container.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import SuggestVideoComponent from "./suggest_video_component";
 
 class SuggestVideoContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   render() {
     return <SuggestVideoComponent />;
   }
