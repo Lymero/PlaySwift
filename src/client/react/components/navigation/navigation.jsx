223f3b236f9bc367397500aaee7d221a1afda637
Valrani <antoinederoose@yahoo.fr>
2018-12-13 11:37:05 +0100

    logo PlaySwift

--- src/client/react/components/navigation/navigation.jsx
+++ src/client/react/components/navigation/navigation.jsx
@@ -1,24 +1,38 @@
 import React from "react";
-import { Nav, Navbar, Button, Image, Dropdown, DropdownButton } from "react-bootstrap";
+import {
+  Nav,
+  Navbar,
+  Button,
+  Image,
+  Dropdown,
+  DropdownButton
+} from "react-bootstrap";
 import { Link } from "react-router-dom";
 import SearchFormContainer from "./search_form_container";
 import Auth from "react/services/auth0";
+import image from "../../../resources/logo.png";
 
 const Navigation = ({ authState, profile }) => {
   return (
     <Navbar bg="dark" variant="dark">
       <Navbar.Brand as={Link} to="/">
-        Home
+        <Image src={image} />
       </Navbar.Brand>
       <Nav className="m-auto">
         <SearchFormContainer />
       </Nav>
-
-      <span className="userIdentification"><Image src={profile.picture} className="" roundedCircle /></span>
-      <DropdownButton  variant="outline-light" id="dropdown-basic-button" title={authState.authenticated ? profile.name : "not logged in"}>
+      <span className="userIdentification">
+        <Image src={profile.picture} className="" roundedCircle />
+      </span>
+      <DropdownButton
+        variant="outline-light"
+        id="dropdown-basic-button"
+        title={authState.authenticated ? profile.name : "not logged in"}
+      >
         <Dropdown.Item href="#/playlists">My Playlists</Dropdown.Item>
         <Dropdown.Item onClick={Auth.logout}>Logout</Dropdown.Item>
-      </DropdownButton>;
+      </DropdownButton>
+      ;
     </Navbar>
   );
 };
